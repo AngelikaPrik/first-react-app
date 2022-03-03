e2d70c200403adc708094924c27b276ed25a9f5e
Angelina <pretzel318@gmail.com>
2022-03-03 13:49:48 +0300

    create todo list

---
+++ src/Modal/Modal.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Modal.css";
+
+export default class Modal extends React.Component {
+	state = {
+		isOpen: false
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				<button onClick={ () => this.setState({ isOpen: true})}>Открыть модальное окно</button>
+				{this.state.isOpen && (
+					<div className="modal">
+					<div className="modal-body">
+						<h1>Модальное окно</h1>
+						<p>Здесь должен быть текст</p>
+						<button onClick={ () => this.setState({ isOpen: false}) }>Закрыть окно</button>
+					</div>
+				</div>
+				)}
+			</React.Fragment>
+		)
+	}
+}
